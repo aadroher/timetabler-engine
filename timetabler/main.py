1d4d504cf4be7314b5b5539e72974f520eb50c9d
Armand Adroher <aadroher@gmail.com>
2020-05-30 17:37:30 +0200

    First steps in adding basic data.

--- timetabler/main.py
+++ timetabler/main.py
@@ -1,10 +1,24 @@
+from yaml import load, Loader as YamlLoader, Dumper as YamlDumper
+from pathlib import Path
+
 from pprint import PrettyPrinter
+
 pp = PrettyPrinter(indent=4).pprint
+
+current_dir = Path(__file__).parent
+data_dir_path = current_dir/'../data'
+studies_data_path = data_dir_path/'studies.yml'
+
+
+def load_data(path):
+    raw_data = path.read_text()
+    return load(raw_data, Loader=YamlLoader)
 
 
 def load_studies():
-    pass
+    data = load_data(studies_data_path)
+    pp(data)
 
 
 def main():
-    print("Hello World!")
+    load_studies()
