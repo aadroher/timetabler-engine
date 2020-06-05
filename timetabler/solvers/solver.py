5db6e49c4d78d5c6ddfc5fe03e953e17d6639a0e
Armand Adroher <aadroher@gmail.com>
2020-06-05 11:15:44 +0200

    Adds bolean variables

--- timetabler/solvers/solver.py
+++ timetabler/solvers/solver.py
@@ -1,10 +1,11 @@
 from itertools import product
+from functools import reduce
 from ortools.sat.python import cp_model
 from ..utils.printer import pp
 from ..models import rooms, days, time_slots, teachers, subjects
 
 
-def solve():
+def get_session_vars():
     rs = rooms.all()
     ds = days.all()
     hs = time_slots.all()
@@ -14,10 +15,24 @@ def solve():
     combinations = product(rs, ds, hs, ts, ss)
     combinations_list = list(combinations)
 
+    # pp(len(combinations_list))
+    # pp(combinations_list[:3])
+
     model = cp_model.CpModel()
 
-    for r, d, h, t, s in combinations_list:
+    def reducer(session_vars, combination):
+        pp(type(combination))
+        r, d, h, t, s = combination
         var_name = '|'.join([item.code for item in [r, d, h, t, s]])
+        new_var = model.NewBoolVar(var_name)
 
-    pp(len(combinations_list))
-    pp(combinations_list[:3])
+        pp(session_vars)
+        pp(var_name)
+        return {**session_vars, combination: new_var}
+
+    return reduce(reducer, combinations_list, dict())
+
+
+def solve():
+    session_vars = get_session_vars()
+    pp(session_vars)
