89ad075647427901ed750e21d2647f3934b99a86
Armand Adroher <aadroher@gmail.com>
2020-06-05 12:27:32 +0200

    WIP

--- timetabler/solvers/solver.py
+++ timetabler/solvers/solver.py
@@ -2,10 +2,10 @@ from itertools import product
 from functools import reduce
 from ortools.sat.python import cp_model
 from ..utils.printer import pp
-from ..models import rooms, days, time_slots, teachers, subjects
+from ..models import rooms, days, time_slots, teachers, subjects, curricula
 
 
-def get_session_vars():
+def get_sessions():
     rs = rooms.all()
     ds = days.all()
     hs = time_slots.all()
@@ -13,26 +13,41 @@ def get_session_vars():
     ss = subjects.all()
 
     combinations = product(rs, ds, hs, ts, ss)
-    combinations_list = list(combinations)
+    return list(combinations)
 
-    # pp(len(combinations_list))
-    # pp(combinations_list[:3])
 
-    model = cp_model.CpModel()
+def get_session_vars(model=None, sessions=[]):
 
-    def reducer(session_vars, combination):
-        pp(type(combination))
-        r, d, h, t, s = combination
-        var_name = '|'.join([item.code for item in [r, d, h, t, s]])
+    def add_session_var(session_vars, session):
+        r, d, h, t, s = session
+        codes = tuple(item.code for item in [r, d, h, t, s])
+        var_name = '|'.join(codes)
         new_var = model.NewBoolVar(var_name)
+        return {**session_vars, codes: new_var}
 
-        pp(session_vars)
-        pp(var_name)
-        return {**session_vars, combination: new_var}
+    return (model, reduce(add_session_var, sessions, {}))
 
-    return reduce(reducer, combinations_list, dict())
+# Rules
+
+
+def separate_room_by_curriculum(model=None, session_vars={}, sessions=[]):
+    cs = curricula.all()
+    cit = next(c for c in cs if c.code == 'cit')
+    hcs = next(c for c in cs if c.code == 'hcs')
+
+    for r, _, _, _, s in sessions:
+        model.Add(r != 'r01' or s in cit.subjects)
+        model.Add(r != 'r02' or s in hcs.subjects)
+
+    return model
 
 
 def solve():
-    session_vars = get_session_vars()
-    pp(session_vars)
+    sessions = get_sessions()
+    model, session_vars = get_session_vars(
+        model=cp_model.CpModel(), sessions=sessions)
+
+    model = separate_room_by_curriculum(
+        model=model, session_vars=session_vars, sessions=sessions)
+
+    print(model.ModelStats())
