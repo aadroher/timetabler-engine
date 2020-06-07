3e431934f85ea60fd4039c344a02f9aa668217f6
Armand Adroher <aadroher@gmail.com>
2020-06-07 18:24:59 +0200

    Makes it work with single subject teachers.

--- timetabler/models/teachers.py
+++ timetabler/models/teachers.py
@@ -1,5 +1,18 @@
-from .loader import load_records
+from .loader import load_records, get_named_tuple
+
+
+def per_subject():
+    subjects = load_records('Subject', 'subjects')
+    return [
+        get_named_tuple('Teacher', {
+            'name': f'Teacher {i}',
+            'code': subject.code,
+            'subjects': (subject.code, )
+        })
+        for i, subject in enumerate(subjects)
+    ]
 
 
 def all():
-    return load_records('Teacher', 'teachers')
+    # return load_records('Teacher', 'teachers')
+    return per_subject()
